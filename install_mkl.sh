3ff3a4d8a7efa118210b157f714bc2192d988b0f
Leow Chee Siang <cheesiangleow5284@gmail.com>
2021-01-23 16:13:00 +0900

    changed build_test.yml use kaldi's install_mkl.sh

--- install_mkl.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y curl gnupg
-curl -sfL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB | apt-key add -
-curl -sfL https://apt.repos.intel.com/setup/intelproducts.list -o /etc/apt/sources.list.d/intelproducts.list
-sudo apt-get update
-sudo apt-get install -y intel-mkl-2020.0.088
