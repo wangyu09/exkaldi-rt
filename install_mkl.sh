098a989ac5361e0e14f0e0347c849f017d3c99c9
Leow Chee Siang <cheesiangleow5284@gmail.com>
2021-01-23 16:10:25 +0900

    modified install_mkl.sh

--- install_mkl.sh
+++ install_mkl.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
-wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-sudo apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
-
-wget https://apt.repos.intel.com/setup/intelproducts.list -O /etc/apt/sources.list.d/intelproducts.list
-
+sudo apt-get install -y curl gnupg
+curl -sfL https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB | apt-key add -
+curl -sfL https://apt.repos.intel.com/setup/intelproducts.list -o /etc/apt/sources.list.d/intelproducts.list
 sudo apt-get update
-sudo apt-get install intel-mkl-2019.0-045
+sudo apt-get install -y intel-mkl-2020.0.088
